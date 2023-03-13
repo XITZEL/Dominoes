c39400765fcd2389e08db2da8a1016f82e8e735b
XITZEL <124205150+XITZEL@users.noreply.github.com>
2023-03-13 19:53:13 +0000

    modificacion 1

---
+++ Dominoes/Program.cs
@@ -0,0 +1,26 @@
+﻿
+class Domino
+{
+ public int d1;
+ public int d2;
+
+ public Domino (int d1, int d2)//Constructor
+ {
+    this.d1 = d1; 
+    this.d2= d2; 
+ }
+ public Domino operator + (Domino a, Domino b)
+ {
+    int d1 = a.d1 * b.d2 ;
+ }
+}
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Domino d1= new Domino(0,1);
+        Domino d2 = new Domino(2,3);
+        int total = d1 + d2; 
+    }
+}
